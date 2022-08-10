fcfe99e0ca3a90ae25a23bd7faa8c3e1a5696a24
WasifAli994 <mwasifali994@gmail.com>
2022-08-10 15:02:05 +0500

    Error handle and async Handler

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -1,34 +1,40 @@
+const asyncHandler = require('express-async-handler');
+
 // @desc Get Tickets
 // @route GET /tickets
 // @access private
 
-const getTickets = (req,res) => {
+const getTickets = asyncHandler(async (req,res) => {
     res.json({message: "Get Tickets"}).status(200);
-}
+})
 
 // @desc Add Ticket
 // @route POST /tickets
 // @access private
 
-const setTickets = (req,res) => {
+const setTickets = asyncHandler(async (req,res) => {
+    if(!req.body.text){
+        res.status(400);
+        throw new Error("Please add a text field!");
+    }
     res.json({message: "Ticket Added"}).status(200);
-}
+})
 
 // @desc Update Ticket
 // @route PUT /tickets/:id
 // @access private
 
-const updateTickets = (req,res) => {
+const updateTickets = asyncHandler(async (req,res) => {
     res.json({message: `Ticket # ${req.params.id} Updated`}).status(200);
-}
+})
 
 // @desc Delete Ticket
 // @route DELETE /tickets
 // @access private
 
-const deleteTickets = (req,res) => {
+const deleteTickets = asyncHandler(async (req,res) => {
     res.json({message: `Ticket # ${req.params.id} Deleted`}).status(200);
-}
+})
 
 module.exports = {
     getTickets,
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const {errorHandler} = require('./middleware/errorMiddleware');
 
 const port = process.env.PORT || 5000;
 
 const app = express();
 
+//Middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
 // => /tickets route has been specified in the middleware so the '/' ca be used in all of the http request methods in routes. 
 app.use('/tickets', require('./routes/ticketRoutes'));
+
+app.use(errorHandler);
 
 app.listen(port,() => {
     console.log(`Server is listening on port: ${port}`);
