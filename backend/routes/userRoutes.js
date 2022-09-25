036c628dd78556d28a14d787f75c379e283273c4
WasifAli994 <mwasifali994@gmail.com>
2022-09-25 13:04:13 +0500

    Added jwt authetication to protect /me route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -8,10 +8,11 @@ const {
 
 } = require('../controllers/userController')
 
+const {protect} = require('../middleware/authMiddleware')
 
 router.post('/', registerUser)
 router.post('/login', loginUser)
-router.get('/me', getMe)
+router.get('/me', protect ,getMe)
 
 
 module.exports = router
